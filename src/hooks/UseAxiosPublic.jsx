ee1071a6c07d9612c8c6d1f331999846578e0581
sazed1237 <sazedulislam9126@gmail.com>
2024-08-25 22:15:04 +0600

    backend url change

--- src/hooks/UseAxiosPublic.jsx
+++ src/hooks/UseAxiosPublic.jsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const UseAxiosPublic = () => {
     const axiosPublic = axios.create({
-        baseURL: import.meta.env.VITE_BASE_URL
-        // baseURL: import.meta.env.VITE_BASE_URL_PRODUCTION
+        // baseURL: import.meta.env.VITE_BASE_URL
+        baseURL: import.meta.env.VITE_BASE_URL_PRODUCTION
     })
 
     return axiosPublic
